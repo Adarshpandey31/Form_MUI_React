179da76d1f1d8c7aa3a8064e24731cc154cdf478
Adarsh Pandey <108523569+Adarshpandey31@users.noreply.github.com>
2023-08-04 03:52:08 +0530

    mobile-responsive code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import Zoom from '@mui/material/Zoom';
-// import { accordionDetailsClasses, colors } from '@mui/material';
 import AlarmIcon from '@mui/icons-material/AccessTimeFilled';
 import DoneIcon from '@mui/icons-material/Done';
 import ButtonGroup from '@mui/material/ButtonGroup';
@@ -17,17 +15,17 @@ function Form() {
   const [showForm, setShowForm] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const transitionKey = `${showForm}-${currentQuestion}`;
-  
+
   const questions = [
-    ["1.) Enter your name*", "input"],
-    ["2.) Enter your brand name*", "input"],
-    ["3.) How satisfied are you with the overall experience of working with upcred.ai*", "rating"],
-    ["4.) Did upcred.ai team effectively understand and address your brand's goals and objectives?*", "tick"],
-    ["5.) How well did our team communicate and collaborate with your brand throughout the campaign?*", "rating"],
-    ["6.) Were the deliverables and content produced by upcred.ai in line with your expectations and brand guidelines?*", "tick"],
-    ["7.) Did the campaign generate the desired results and meet your marketing objectives?*", "tick"],
-    ["8.) How likely are you to recommend upcred.ai to other brands based on your experience?*", "rating"],
-    ["9.) Would you consider partnering with upcred.ai again for future campaigns?*", "tick"],
+    ["Enter your name*", "input"],
+    ["Enter your brand name*", "input"],
+    ["How satisfied are you with the overall experience of working with upcred.ai*", "rating"],
+    ["Did upcred.ai team effectively understand and address your brand's goals and objectives?*", "tick"],
+    ["How well did our team communicate and collaborate with your brand throughout the campaign?*", "rating"],
+    ["Were the deliverables and content produced by upcred.ai in line with your expectations and brand guidelines?*", "tick"],
+    ["Did the campaign generate the desired results and meet your marketing objectives?*", "tick"],
+    ["How likely are you to recommend upcred.ai to other brands based on your experience?*", "rating"],
+    ["Would you consider partnering with upcred.ai again for future campaigns?*", "tick"],
   ];
 
   const handleInputChange = (e) => {
@@ -43,8 +41,9 @@ function Form() {
     } else {
       if (showForm) {
         // If form is visible, hide it
-        setCurrentQuestion(0);
         setShowForm(false);
+        setCurrentQuestion(0);
+
       } else {
         // If form is already hidden, submit the data or perform any other action
         console.log(formData);
@@ -61,65 +60,121 @@ function Form() {
         classNames="form-fade-out"
         unmountOnExit
       >
-        <div className="title">
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          textAlign="center"
+          justifyContent="center"
+          minHeight="100vh"
+          fontSize={22}
+          padding="0"
+          margin="0"
+          left={0}
+          sx={{
+            fontFamily: ['arial'],
+            '@media (max-width: 700px)': {
+              fontSize: 18,
+            },
+            '@media (max-width: 450px)': {
+              fontSize: 16,
+            },
+          }}
+        >
           <Box
+            marginBottom={3}
+            // border={1}
+            sx={{
+              fontFamily: ['arial'],
+              '@media (max-width: 700px)': {
+                fontSize: 18,
+              },
+              '@media (max-width: 450px)': {
+                fontSize: 16,
+              },
+              marginBottom: 2,
+            }}>
+            Brand Feedback Form
+          </Box>
+          <Box
+            marginBottom={3}
+            fontStyle={blur}
+            fontWeight={1}
+            sx={{ opacity: [0.5], marginBottom: 2.5, }}
+          // border={1}
+          >
+            We are committed to delivering the best experiences
+          </Box>
+          <Box
+            mt={1}
             display="flex"
-            flexDirection="column"
+            flexDirection="row"
             alignItems="center"
             justifyContent="center"
-            minHeight="100vh"
-            fontSize={22}
-            padding="0"
-            margin="0"
-            sx={{ fontFamily: ['arial'] }}>
+            textAlign="center"
+            paddingLeft={12}
+            // border={1}
+            sx={{
+              fontFamily: ['arial'],
+              '@media (max-width: 700px)': {
+                fontSize: 16,
+                paddingLeft: 0,
+                bottom: 0,
+                marginTop: 0,
+              },
+            }}>
+            <Button sx={{
+              '@media (max-width: 700px)': { fontSize: 15, bottom: 0, },
+              '@media (max-width: 450px)': {
+                fontSize: 13,
+              },
+              padding: [1],
+              fontSize: [18],
+              fontWeight: ['bold'],
+              fontFamily: ['arial']
+            }}
+              variant="contained"
+              onClick={() => setShowForm(true)}>
+              Tell us what you think
+            </Button>
             <Box
-              marginBottom={3}>
-              Brand Feedback Form
-            </Box>
-            <Box
-              marginBottom={3}
-              fontStyle={blur}
-              fontWeight={1}
-              sx={{ opacity: [0.5] }}>
-              We are committed to delivering the best experiences
-            </Box>
-            <Box
-              mt={1}
-              display="flex"
-              flexDirection="row"
-              alignItems="center"
-              justifyContent="right"
-              paddingLeft={12}
+              sx={{
+                padding: [1],
+                fontSize: [15],
+                '@media (max-width: 700px)': { display: "none", },
+              }}
+            // border={1}
             >
-              <Button sx={{ padding: [1.1], fontSize: [18], fontWeight: ['bold'], fontFamily: ['arial'] }} variant="contained" onClick={() => setShowForm(true)}>
-                Tell us what you think
-              </Button>
-              <Box
-                sx={{ padding: [1], fontSize: [15] }}>
-                click here ↵
-              </Box>
-            </Box>
-            <Box
-              mt={1}
-              sx={{ fontSize: [15] }}
-              display="flex"
-              flexDirection="row"
-              alignItems="center"
-              justifyContent="center">
-              <AlarmIcon sx={{ fontSize: [17], marginRight: [0.5] }} />
-              Takes 1 minute
+              click here ↵
             </Box>
           </Box>
-        </div >
+          <Box
+            mt={1}
+            // fontSize="15"
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="center"
+            // border={1}
+            sx={{
+              fontSize: [15],
+              '@media (max-width: 450px)': {
+                fontSize: 14,
+              },
+            }}>
+            <AlarmIcon sx={{ fontSize: [17], marginRight: [0.5], '@media (max-width: 700px)': { fontSize: 15, }, }} />
+            Takes 1 minute
+          </Box>
+        </Box>
+
       </CSSTransition>
 
-      <div className="title">
       <TransitionGroup>
         {showForm && (
           <CSSTransition
             key={transitionKey}
             in={true}
-            timeout={300}
+            timeout={600}
             classNames="question-transition"
             unmountOnExit
           >
@@ -131,13 +186,49 @@ function Form() {
               paddingLeft={6}
               paddingRight={6}
               minHeight="100vh"
-              fontSize={23}
               fontFamily="arial"
+              border={1}
+              sx={{
+                fontSize: [18],
+                '@media (max-width: 1000px)': {
+                  fontSize: 16, padding: 0,
+                },
+                '@media (max-width: 700px)': {
+                  fontSize: 16,
+                  paddingLeft: 2,
+                  paddingRight: 2,
+                },
+                '@media (max-width: 350px)': {
+                  fontSize: 16,
+                  paddingLeft: 0.5,
+                  paddingRight: 0.5,
+                },
+              }}
             >
               <Box mb={2.5}
-                fontSize={23}
+                // fontSize={23}
                 paddingLeft={20}
                 paddingRight={20}
+                // border={1}
+                sx={{
+                  '@media (max-width: 1000px)': {
+                    fontSize: 18,
+                    padding: 0,
+                    paddingLeft: 5,
+                    paddingRight: 5,
+                  },
+                  '@media (max-width: 700px)': {
+                    fontSize: 16,
+                    padding: 0,
+                    paddingLeft: 2,
+                    paddingRight: 2,
+                  },
+                  '@media (max-width: 350px)': {
+                    fontSize: 15,
+                    paddingLeft: 0.5,
+                    paddingRight: 0.5,
+                  },
+                }}
               >
                 {questions[currentQuestion][0]}
               </Box>
@@ -148,6 +239,25 @@ function Form() {
                 justifyContent="center"
                 paddingLeft={20}
                 paddingRight={20}
+                // border={1}
+                sx={{
+                  '@media (max-width: 1000px)': {
+                    fontSize: 18,
+                    padding: 0,
+                    paddingLeft: 5,
+                    paddingRight: 5,
+                  },
+                  '@media (max-width: 700px)': {
+                    fontSize: 16,
+                    paddingLeft: 2,
+                    paddingRight: 2,
+                  },
+                  '@media (max-width: 350px)': {
+                    fontSize: 14,
+                    paddingLeft: 0.5,
+                    paddingRight: 0.5,
+                  },
+                }}
               >
                 {questions[currentQuestion][1] === "input" &&
                   (<TextField
@@ -159,12 +269,16 @@ function Form() {
                     label={"Type your answer here..."}
                   />)}
                 {questions[currentQuestion][1] === "rating" &&
-                  (<ButtonGroup fullWidth={true} variant="outlined" aria-label="outlined button group">
-                    <Button>1</Button>
-                    <Button>2</Button>
-                    <Button>3</Button>
-                    <Button>4</Button>
-                    <Button>5</Button>
+                  (<ButtonGroup fullWidth={true}
+                    variant="outlined"
+                    aria-label="outlined button group"
+                    onClick={handleNextQuestion}
+                  >
+                    <Button key="1">1</Button>
+                    <Button key="2">2</Button>
+                    <Button key="3">3</Button>
+                    <Button key="4">4</Button>
+                    <Button key="5">5</Button>
                   </ButtonGroup>
                   )}
                 {questions[currentQuestion][1] === "tick" &&
@@ -173,11 +287,62 @@ function Form() {
                     flexDirection="column"
                     alignItems="left"
                     justifyContent="center"
-                    textAlign="center">
-                    <Button startIcon={< DoneIcon />} sx={{ backgroundColor: ["rgb(4,95,167, 0.1)"], textAlign: ["center"], alignItems: ["center"], marginBottom: [1], color: ["rgb(4,95,167)"], border: [1], width: ["16%"], paddingLeft: [4], paddingRight: [4], fontSize: [18], fontFamily: ['arial'] }} variant="contained" onClick={handleNextQuestion}>
+                    textAlign="center"
+                    sx={{
+                      '@media (max-width: 1000px)': {
+                        fontSize: 16,
+                        marginTop: 1,
+                      },
+                      '@media (max-width: 700px)': {
+                        fontSize: 16,
+                        marginTop: 0,
+                      },
+                    }}
+                  >
+                    <Button startIcon={< DoneIcon />}
+                      sx={{
+                        backgroundColor: ["rgb(4,95,167, 0.1)"],
+                        marginBottom: [1],
+                        color: ["rgb(4,95,167)"],
+                        border: [1],
+                        width: [140],
+                        height: 40,
+                        fontSize: [18],
+                        fontFamily: ['arial'],
+                        '@media (max-width: 700px)': {
+                          fontSize: 16,
+                          width: 120,
+                          height: 35,
+                        },
+                        '@media (max-width: 600px)': {
+                          width: 120,
+                          height: 35,
+                          fontSize: 15,
+                        },
+                      }} variant="contained" onClick={handleNextQuestion}
+                    >
                       Yes
                     </Button>
-                    <Button startIcon={<CloseIcon />} sx={{ backgroundColor: ["rgb(4,95,167, 0.1)"], color: ["rgb(4,95,167)"], border: [1], width: ["16%"], fontSize: [18], fontFamily: ['arial'] }} variant="contained" onClick={handleNextQuestion}>
+                    <Button startIcon={<CloseIcon />}
+                      sx={{
+                        backgroundColor: ["rgb(4,95,167, 0.1)"],
+                        color: ["rgb(4,95,167)"],
+                        border: [1],
+                        width: [140],
+                        fontSize: [18],
+                        height: 38,
+                        fontFamily: ['arial'],
+                        '@media (max-width: 700px)': {
+                          fontSize: 16,
+                          width: 120,
+                          height: 35,
+                        },
+                        '@media (max-width: 600px)': {
+                          width: 120,
+                          height: 35,
+                          fontSize: 15,
+                        },
+                      }} variant="contained" onClick={handleNextQuestion}>
                       No
                     </Button>
                   </Box>
@@ -187,12 +352,42 @@ function Form() {
                   flexDirection="row"
                   alignItems="center"
                   justifyContent="left"
+                // border={1}
                 >
-                  <Button endIcon={< DoneIcon fontSize='large' />} sx={{ paddingLeft: [2], paddingRight: [2], fontSize: [18], fontWeight: ['bold'], fontFamily: ['arial'] }} variant="contained" onClick={handleNextQuestion}>
+                  <Button endIcon={< DoneIcon fontSize='large' />}
+                    sx={{
+                      paddingLeft: [2],
+                      paddingRight: [2],
+                      fontSize: [18],
+                      fontWeight: ['bold'],
+                      fontFamily: ['arial'],
+                      '@media (max-width: 1000px)': {
+                        fontSize: 16,
+                      },
+                      '@media (max-width: 700px)': {
+                        fontSize: 15,
+                        paddingLeft: 1.5,
+                        paddingRight: 1.5,
+                      },
+                      '@media (max-width: 350px)': {
+                        fontSize: 14,
+                        paddingLeft: 0.5,
+                        paddingRight: 0.5,
+                      },
+                    }}
+                    variant="contained"
+                    onClick={handleNextQuestion}
+                  >
                     OK
                   </Button>
                   <Box
-                    sx={{ padding: [1], fontSize: [15] }}>
+                    sx={{
+                      padding: [1],
+                      fontSize: [16],
+                      '@media (max-width: 700px)': {
+                        fontSize: 14,
+                      },
+                    }}>
                     click here ↵
                   </Box>
                 </Box>
@@ -201,7 +396,6 @@ function Form() {
           </CSSTransition>
         )}
       </TransitionGroup>
-      </div>
     </>
   );
 }
