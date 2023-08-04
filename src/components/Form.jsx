917bf121051c84f83458cee823380f5a97a5569d
Adarsh Pandey <108523569+Adarshpandey31@users.noreply.github.com>
2023-08-04 13:47:30 +0530

    added dark mode working completely fine

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,6 +5,10 @@ import Box from '@mui/material/Box';
 import AlarmIcon from '@mui/icons-material/AccessTimeFilled';
 import DoneIcon from '@mui/icons-material/Done';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 // import { CheckBox } from '@mui/icons-material';
 // import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import CloseIcon from '@mui/icons-material/Close';
@@ -15,6 +19,7 @@ function Form() {
   const [showForm, setShowForm] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const transitionKey = `${showForm}-${currentQuestion}`;
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
   const questions = [
     ["Enter your name*", "input"],
@@ -52,122 +57,140 @@ function Form() {
     }
   };
 
+  //for light and dark mode
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
     <>
-      <CSSTransition
-        in={!showForm}
-        timeout={600}
-        classNames="form-fade-out"
-        unmountOnExit
-      >
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          textAlign="center"
-          justifyContent="center"
-          minHeight="100vh"
-          fontSize={22}
-          padding="0"
-          margin="0"
-          left={0}
-          sx={{
-            fontFamily: ['arial'],
-            '@media (max-width: 700px)': {
-              fontSize: 18,
-            },
-            '@media (max-width: 450px)': {
-              fontSize: 16,
-            },
-          }}
+    <ThemeProvider theme  ={theme}>
+    <CssBaseline/>
+      {!showForm && (
+        <CSSTransition
+          in={!showForm}
+          timeout={600}
+          classNames="form-fade-out"
+          unmountOnExit
         >
           <Box
-            marginBottom={3}
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            textAlign="center"
+            justifyContent="center"
+            minHeight="100vh"
+            // position="fixed"
+            // bottom={0}
+            // left={0}
+            // right={0}
+            // top={0}
+            fontSize={22}
+            padding="0"
+            margin="0"
             // border={1}
             sx={{
               fontFamily: ['arial'],
+              overflow: "hidden",
               '@media (max-width: 700px)': {
                 fontSize: 18,
               },
               '@media (max-width: 450px)': {
                 fontSize: 16,
               },
-              marginBottom: 2,
-            }}>
-            Brand Feedback Form
-          </Box>
-          <Box
-            marginBottom={3}
-            fontStyle={blur}
-            fontWeight={1}
-            sx={{ opacity: [0.5], marginBottom: 2.5, }}
-          // border={1}
-          >
-            We are committed to delivering the best experiences
-          </Box>
-          <Box
-            mt={1}
-            display="flex"
-            flexDirection="row"
-            alignItems="center"
-            justifyContent="center"
-            textAlign="center"
-            paddingLeft={12}
-            // border={1}
-            sx={{
-              fontFamily: ['arial'],
-              '@media (max-width: 700px)': {
-                fontSize: 16,
-                paddingLeft: 0,
-                bottom: 0,
-                marginTop: 0,
-              },
-            }}>
-            <Button sx={{
-              '@media (max-width: 700px)': { fontSize: 15, bottom: 0, },
-              '@media (max-width: 450px)': {
-                fontSize: 13,
-              },
-              padding: [1],
-              fontSize: [18],
-              fontWeight: ['bold'],
-              fontFamily: ['arial']
             }}
-              variant="contained"
-              onClick={() => setShowForm(true)}>
-              Tell us what you think
-            </Button>
+          >
             <Box
+              marginBottom={3}
+              // border={1}
               sx={{
-                padding: [1],
-                fontSize: [15],
-                '@media (max-width: 700px)': { display: "none", },
-              }}
+                fontFamily: ['arial'],
+                '@media (max-width: 700px)': {
+                  fontSize: 18,
+                },
+                '@media (max-width: 450px)': {
+                  fontSize: 16,
+                },
+                marginBottom: 2,
+              }}>
+              Brand Feedback Form
+            </Box>
+            <Box
+              marginBottom={3}
+              fontStyle={blur}
+              fontWeight={1}
+              sx={{ opacity: [0.5], marginBottom: 2.5, }}
             // border={1}
             >
-              click here ↵
+              We are committed to delivering the best experiences
+            </Box>
+            <Box
+              mt={1}
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+              justifyContent="center"
+              textAlign="center"
+              paddingLeft={12}
+              // border={1}
+              sx={{
+                fontFamily: ['arial'],
+                '@media (max-width: 700px)': {
+                  fontSize: 16,
+                  paddingLeft: 0,
+                  bottom: 0,
+                  marginTop: 0,
+                },
+              }}>
+              <Button sx={{
+                '@media (max-width: 700px)': { fontSize: 15, bottom: 0, },
+                '@media (max-width: 450px)': { fontSize: 13, },
+                padding: [1],
+                fontSize: [18],
+                fontWeight: ['bold'],
+                fontFamily: ['arial']
+              }}
+                variant="contained"
+                onClick={() => setShowForm(true)}>
+                Tell us what you think
+              </Button>
+              <Box
+                sx={{
+                  padding: [1],
+                  fontSize: [15],
+                  '@media (max-width: 700px)': { display: "none", },
+                }}
+              // border={1}
+              >
+                click here ↵
+              </Box>
+            </Box>
+            <Box
+              mt={1}
+              // fontSize="15"
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+              justifyContent="center"
+              // border={1}
+              sx={{
+                fontSize: [15],
+                '@media (max-width: 450px)': {
+                  fontSize: 14,
+                },
+              }}>
+              <AlarmIcon sx={{ fontSize: [17], marginRight: [0.5], '@media (max-width: 700px)': { fontSize: 15, }, }} />
+              Takes 1 minute
             </Box>
           </Box>
-          <Box
-            mt={1}
-            // fontSize="15"
-            display="flex"
-            flexDirection="row"
-            alignItems="center"
-            justifyContent="center"
-            // border={1}
-            sx={{
-              fontSize: [15],
-              '@media (max-width: 450px)': {
-                fontSize: 14,
-              },
-            }}>
-            <AlarmIcon sx={{ fontSize: [17], marginRight: [0.5], '@media (max-width: 700px)': { fontSize: 15, }, }} />
-            Takes 1 minute
-          </Box>
-        </Box>
-
-      </CSSTransition>
+        </CSSTransition>
+      )}
 
       <TransitionGroup>
         {showForm && (
@@ -185,11 +208,18 @@ function Form() {
               justifyContent="center"
               paddingLeft={6}
               paddingRight={6}
-              minHeight="100vh"
+              // position="fixed"
+              // bottom={0}
+              // left={0}
+              // right={0}
+              // top={0}
+              height="100vh"
               fontFamily="arial"
-              border={1}
+              margin={0}
+              // border={1}
               sx={{
                 fontSize: [18],
+                overflow: "hidden",
                 '@media (max-width: 1000px)': {
                   fontSize: 16, padding: 0,
                 },
@@ -396,6 +426,7 @@ function Form() {
           </CSSTransition>
         )}
       </TransitionGroup>
+      </ThemeProvider>
     </>
   );
 }
